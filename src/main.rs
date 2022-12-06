6249524af6e7512a5a8fe8ad4851821c4dcb4d07
Michael Scholten <michael-scholten@hotmail.nl>
2022-12-06 19:03:16 +0100

    Removed all JavaScript, christmas pagage now uses server side rendering.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![deny(clippy::panic, clippy::panic_in_result_fn,clippy::panicking_unwrap,
+    non_fmt_panics, unconditional_panic, unsafe_code)]
 #![warn(clippy::pedantic, clippy::nursery)]
 #![feature(async_closure)]
 
@@ -5,20 +7,58 @@ use std::net::SocketAddr;
 
 use axum::{Router, routing::get, response::{Html, IntoResponse}, http::{HeaderMap, header}};
 use tokio::fs::read;
+use chrono::prelude::*;
 
 async fn read_file(filename: String) -> Vec<u8>{
     read(filename).await.unwrap_or_else(|error| error.to_string().bytes().collect())
 }
 
+fn christmas() -> String{
+    let now = chrono::Local::now();
+    let first_christmas_day = chrono::Local.with_ymd_and_hms(2022, 12, 25, 0, 0, 0).unwrap();
+    let second_christmas_day = chrono::Local.with_ymd_and_hms(2022, 12, 26, 0, 0, 0).unwrap();
+    let next_year = chrono::Local.with_ymd_and_hms(2023, 1, 1, 0, 0, 0).unwrap();
+    let mut page = r##"<!DOCTYPE html>
+<html>
+    <head>
+        <title>Merry Christmas and a happy new year</title>
+        <link rel="stylesheet" href="/standard.css">
+    </head>
+    <body>
+        <div id="content">
+            <h1>Merry Christmas and a happy new year!</h1>
+            <p>Als je dit leest, wil ik zeggen dat je een plek in mijn hart hebt.<br/>
+                Dit is omdat je een goede vriend of naast familielid bent.<br/>
+                Daarom wens ik je een fijne kerst en een gelukkig en gezond nieuw jaar.<br/>
+            <strong>"##.to_owned();
+
+    if now < first_christmas_day{
+        page += &format!("Dagen tot eerste kerstdag: {}<br/>", (first_christmas_day - now).num_days() + 1);
+    }
+    if now < second_christmas_day{
+        page += &format!("Dagen tot tweede kerstdag: {}<br/>", (second_christmas_day - now).num_days() + 1);
+    }
+    if (next_year - now).num_days() > 0{
+        page += &format!("Dagen tot 2023: {}<br/>", (next_year - now).num_days() + 1);
+    }else if now < next_year{
+        let time_till_next_year = next_year - now;
+        page += &format!("{}:{}:{} tot 2023<br/>", time_till_next_year.num_hours(), time_till_next_year.num_minutes(), time_till_next_year.num_seconds());
+    }else{
+        page += &format!("Gelukkig 2023!<br/>");
+    }
+    page += r"</strong>
+            <p>groetjes,</p>
+            <p>Michael Scholten en Joy</p>
+        </div>
+    </body>
+</html>";
+
+    page
+}
+
 fn css(content: Vec<u8>) -> impl IntoResponse{
     let mut headers = HeaderMap::new();
     headers.insert(header::CONTENT_TYPE, "text/css".parse().unwrap());
-    (headers, content)
-}
-
-fn javascript(content: Vec<u8>) -> impl IntoResponse{
-    let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "text/javascript".parse().unwrap());
     (headers, content)
 }
 
@@ -35,21 +75,15 @@ fn pdf(content: Vec<u8>) -> impl IntoResponse{
 }
 
 fn add_html_pages(mut app: Router) -> Router{
-    let routes = vec!["/", "/zakelijk.html", "/technisch.html", "/algemeen.html", "/christmas.html"];
+    let routes = vec!["/", "/zakelijk.html", "/technisch.html", "/algemeen.html", "/kerst.html"];
     for route in routes{
         if route == "/"{
             app = app.route(route,  get(async || Html(read_file("src/html/index.html".to_owned()).await)));
+        }else if route == "/kerst.html"{
+            app = app.route(route,  get(async || Html(christmas())));
         }else{
             app = app.route(route,  get(async || Html(read_file("src/html".to_owned() + route).await)));
         }
-    }
-    app
-}
-
-fn add_javascript(mut app: Router) -> Router{
-    let routes = vec!["/countdown.js"];
-    for route in routes{
-        app = app.route(route,  get(async || javascript(read_file("src/js".to_owned() + route).await)));
     }
     app
 }
@@ -115,7 +149,6 @@ async fn main() {
     // build the application
     let mut app = Router::new();
     app = add_html_pages(app);
-    app = add_javascript(app);
     app = add_css(app);
     app = add_images(app);
     app = add_videos(app);
