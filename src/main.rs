0fda6c95b74cf2b5d8b55797151c1e549cdd4cf0
Michael Scholten <michael-scholten@hotmail.nl>
2022-12-10 19:13:22 +0100

    Added https functionality.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 #![feature(async_closure)]
 
-use std::net::SocketAddr;
-
-use axum::{Router, routing::get, response::{Html, IntoResponse}, http::{HeaderMap, header}};
-use tokio::fs::read;
-use chrono::prelude::*;
+use axum::{routing::get, Router, response::{IntoResponse, Html}};
+use chrono::TimeZone;
+use futures_util::future::poll_fn;
+use hyper::{server::{conn::{Http, AddrIncoming}, accept::Accept}, HeaderMap, header};
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use std::{
+    fs::File,
+    io::BufReader,
+    net::SocketAddr,
+    path::{Path, PathBuf},
+    pin::Pin,
+    sync::Arc,
+};
+use tokio::{net::TcpListener, fs::read};
+use tokio_rustls::{
+    rustls::{Certificate, PrivateKey, ServerConfig},
+    TlsAcceptor,
+};
+use tower::MakeService;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 async fn read_file(filename: String) -> Vec<u8>{
     read(filename).await.unwrap_or_else(|error| error.to_string().bytes().collect())
@@ -147,8 +162,52 @@ fn add_others(mut app: Router) -> Router{
     app
 }
 
+fn rustls_server_config(key: impl AsRef<Path>, cert: impl AsRef<Path>) -> Arc<ServerConfig>{
+    let mut key_reader = BufReader::new(File::open(key).unwrap());
+    let mut cert_reader = BufReader::new(File::open(cert).unwrap());
+
+    let key = PrivateKey(pkcs8_private_keys(&mut key_reader).unwrap().remove(0));
+    let certs = certs(&mut cert_reader).unwrap()
+        .into_iter().map(Certificate).collect();
+    
+    let mut config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .expect("bad certificate/key");
+    
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
+
+    Arc::new(config)
+}
+
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_tls_rustls=debug".into())
+        ).with(tracing_subscriber::fmt::layer())
+        .init();
+    
+    #[cfg(target_arch="aarch64")]
+    let rustls_config = rustls_server_config(
+        PathBuf::from("/etc/letsencrypt/live/michaeljoy.nl/privkey.pem"),
+        PathBuf::from("/etc/letsencrypt/live/michaeljoy.nl/fullchain.pem")
+    );
+
+    #[cfg(not(target_arch="aarch64"))]
+    let rustls_config = rustls_server_config(
+        PathBuf::from("../michaeljoy_certificates/key.pem"),
+        PathBuf::from("../michaeljoy_certificates/certificate.pem")
+    );
+
+    let acceptor = TlsAcceptor::from(rustls_config);
+    let listener = TcpListener::bind("0.0.0.0:80").await.unwrap();
+    let mut listener = AddrIncoming::from_listener(listener).unwrap();
+
+    let protocol = Arc::new(Http::new());
+
     // build the application
     let mut app = Router::new();
     app = add_html_pages(app);
@@ -158,7 +217,20 @@ async fn main() {
     app = add_pdf(app);
     app = add_others(app);
 
-    let address = SocketAddr::from(([0, 0, 0, 0], 80));
-    axum::Server::bind(&address)
-        .serve(app.into_make_service()).await.unwrap();
+    let mut app = app.into_make_service_with_connect_info::<SocketAddr>();
+
+    loop{
+        let stream = poll_fn(|cx| Pin::new(&mut listener).poll_accept(cx))
+            .await.unwrap().unwrap();
+        
+        let acceptor = acceptor.clone();
+        let protocol = protocol.clone();
+        let svc = app.make_service(&stream);
+
+        tokio::spawn(async move{
+            if let Ok(stream) = acceptor.accept(stream).await{
+                let _ = protocol.serve_connection(stream, svc.await.unwrap()).await;
+            }
+        });
+    }
 }
