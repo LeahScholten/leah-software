fb91b65f97fa76256b7a763ff95d437d916d133f
Michael Scholten <michael-scholten@hotmail.nl>
2024-04-11 09:31:02 +0100

    Fixed canvas size and text color, added actions to perform for certain moods.

--- src/js/mood.js
+++ src/js/mood.js
@@ -5,8 +5,8 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(512, 1024);
     thermometer.resize(512, 0);
+    createCanvas(thermometer.width, thermometer.height);
     image(thermometer, 0, 0);
     // Min 550
     // Max 50
